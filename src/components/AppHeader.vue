717ba7c28e10cab65b9a3c71d559743095c1bc3e
Paolacocc <coccettipaola@gmail.com>
2023-04-26 14:53:35 +0200

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+    name: "AppHeader",
+    data() {
+        return {
+            HeaderLinks: [
+                {
+                    title: "home",
+                    image: ""
+                },
+                {
+                    title:
+                    image
+                },
+                {
+                    title:
+                    image
+                },
+                {
+                    title:
+                    image
+                },
+                {
+                    title:
+                    image
+                },
+                {
+                    title:
+                    image
+                },
+                {
+                    title:
+                    image
+                }
+                
+            ]
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div class="header">
+       
+    </div>
+    
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/AppLinks.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: "AppLinks",
+    props: {
+        title: String,
+        image: String,
+    }
+
+}
+
+</script>
+
+<template>
+    <div class="header-links" v-if="title === ''">
+        <img :src="image" alt="">
+    </div>
+    <div class="header-links" v-else-if="image === ''">
+        <h6 class="title">{{ title }}</h6>
+    </div>
+    <div class="header-links" v-else>
+        <img :src="image" alt="">
+        <h6 class="title">{{ title }}</h6>
+    </div>
+</template>
+
+<style scoped></style>
